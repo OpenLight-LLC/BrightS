c2f92d6b53914c237863fd0c9167028452f5e3e7
s12mcOvO <3549287757@qq.com>
2026-03-26 00:20:30 +0800

    🖋 Edit the kshell.c

--- build-brights/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/s12mc/CodeSpace/brights
-# Build directory: /home/s12mc/CodeSpace/brights/build-brights
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("sys")
-subdirs("tests")
